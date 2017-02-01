a5412b341e043ec95be33aece438a32d693b6fd6
scra3 <albanbertolini@gmail.com>
2017-02-01 17:28:30 +0100

    save q1

---
+++ ARO/TP2/sacAdos/CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: b16007026
+ *
+ * Created on 1 février 2017, 14:21
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
