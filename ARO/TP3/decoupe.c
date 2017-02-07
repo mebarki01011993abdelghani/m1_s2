a62deeb39216da3408502aa1980b58daf2d73957
scra3 <albanbertolini@gmail.com>
2017-02-07 22:24:05 +0100

    debut c

--- ARO/TP3/decoupe.c
+++ ARO/TP3/decoupe.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glpk.h>
+#include <stdbool.h>
 
-struct Contrainte{
-	int largeur;
-	int quantite;
-}c1;
 
-struct Decoupe {
-	int motifs[4];
-	c1;
+bool check_contrainte_taille_rouleau(int *motifs,int *tailles, int taille_rouleau){
+	int somme = 0;
+	int tailleMotifs = sizeof(motifs)/sizeof(int);
+	for(int i = 0; i < tailleMotifs;i++){
+		somme = somme + motifs[i] * tailles[i];
+	}
+	return (somme > taille_rouleau) ? false : true;
 };
 
+bool check_contrainte_dual(int *opti_dual,int *tailles){
+	int somme = 0;
+	int tailleOpti = sizeof(opti_dual)/sizeof(int);
+	for(int i = 0; i < tailleOpti;i++){
+		somme = somme + opti_dual[i] * tailles[i];
+	}
+	return (somme > 1) ? true : false;
+};
 
 int main(int argc, char *argv[])
 {
+	int taille_rouleau = 100; // cm : largeur des rouleaux initiaux
+	int motifs[4];
+	int tailles[] = {45,36,31,14}; 
+	int quantitees[] = {97,610,395,211};
+	
 
 exit(0);
 
-}
+};
