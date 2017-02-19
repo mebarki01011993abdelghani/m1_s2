2338f5763933526bf07fe5266cce83ce56d5e9ee
scra3 <albanbertolini@gmail.com>
2017-02-19 09:59:08 +0100

    prog p

--- PROG_PAR/cour3/TPC/src/tpc/Pot_des_sauvages/Diner.java
+++ PROG_PAR/cour3/TPC/src/tpc/Pot_des_sauvages/Diner.java
@@ -1,3 +1,4 @@
+package tpc.Pot_des_sauvages;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -62,16 +63,19 @@ class Cuisinier extends Thread {
 
 class Pot {
 
-    private volatile int nbPortions;
-    private int nbPortionsMax;
-    private volatile boolean wakeUp;
+    private volatile int nbPortions; // nombre de portions du pot restante
+    private int nbPortionsMax; // nombre de portion que peut contenir un pot
+    private volatile boolean wakeUp; // boolean réveillant le cuisinier
+    private static volatile Sauvage louche; // permet de savoir qui à la louche
 
+    //Constructeur du pot
     public Pot(int nbPortions) {
         this.nbPortions = nbPortions;
         this.nbPortionsMax = nbPortions;
         this.wakeUp = false;
     }
 
+    //GET AND SET
     public int getNbPortions() {
         return nbPortions;
     }
@@ -80,6 +84,7 @@ class Pot {
         this.nbPortions = nbPortions;
     }
 
+    // Remplir est la procédure du cuisinier qui permet de remplir de le pot quand le cuisinier est réveillé et que le pot soit vide
     public synchronized void remplir() {
         while (wakeUp == false || nbPortions > 0) { // on attend que le plat soit vide
             try {
@@ -94,30 +99,54 @@ class Pot {
         nbPortions = nbPortionsMax;
         System.out.println("REGALEZ VOUS : pot plein !!");
 
+        // On réveil les sauvages
         notifyAll();
     }
 
+    //Procédure permettant aux sauvages de se servir.Ils peuvent réveiller le cuisinier si le pot est vide.
     public synchronized void seServir() {
-        System.out.println(Thread.currentThread().getName() + " : " + "Je prends la louche ");
 
         while (nbPortions == 0) {   // on attend que le plat soit à de nouveau plein         
             try {
+                //On réveil le cuisinier une seul fois
                 if (wakeUp == false) {
+                    // Le sauvage prend la louche, seuelement lui peut se sevir
+                    louche = (Sauvage) Thread.currentThread();
+                    System.out.println(Thread.currentThread().getName() + " : " + "Je prends la louche ");
+                    louche = (Sauvage) Thread.currentThread();
                     System.out.println("Le pot est vide !");
                     System.out.println(Thread.currentThread().getName() + " : " + "je reveille le cuisinier ");
                     wakeUp = true;
                     System.out.println(Thread.currentThread().getName() + " : " + "J'attends que le pôt soit plein ");
                 }
+                //Ici on réveil seulement le cuisinier
                 notifyAll();
                 wait();
             } catch (InterruptedException ex) {
                 Logger.getLogger(Pot.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+
+        // le while permet d'empecher les sauvages de récupérer la louche si elle est prise
+        while (louche != (Sauvage) Thread.currentThread()) {
+            try {
+                wait();
+                if (louche == null) {
+                    // le sauvage a la louche seulement lui peut se servir
+                    louche = (Sauvage) Thread.currentThread();
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Pot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        System.out.println(Thread.currentThread().getName() + " : " + "Je prends la louche ");
         wakeUp = false;
         System.out.println("Il y a une part de disponible ");
         nbPortions = nbPortions - 1;
         System.out.println(Thread.currentThread().getName() + " : " + "Je pose la louche ");
+        // Le sauvage perd la louche un autre peut se servir
+        louche = null;
         notifyAll();
     }
 }
