c45cd6ff8bcadceb994285bb3452cb7ff4787b35
scra3 <albanbertolini@gmail.com>
2017-03-02 10:44:22 +0100

    ok

--- XML/PROJET/TIDY/verif.sh
+++ XML/PROJET/TIDY/verif.sh
@@ -1,5 +1,5 @@
 #/!bin/bash
-for FILE in `find www/ -name "*html" `;
+for FILE in `find ../www/ -name "*html" `;
 do
 	execPath=$PWD/${FILE}
 	executionTidy="tidy -q --show-warnings false -utf8 $execPath"
