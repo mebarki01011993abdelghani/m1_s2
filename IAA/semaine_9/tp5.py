1aceb9787480cd6650a9b6a702fa2df5de645932
scra3 <albanbertolini@gmail.com>
2017-04-03 21:18:10 +0200

    iaa

--- IAA/semaine_9/tp5.py
+++ IAA/semaine_9/tp5.py
@@ -9,6 +9,8 @@ import random
 from sklearn.datasets import make_classification
 from sklearn.cross_validation import train_test_split
 from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+	
 
 
 
@@ -41,7 +43,7 @@ def question121() :
 	reseau.fit(X_train,Y_train)
 	resultat = reseau.predict(X_test)
 	parametres = reseau.get_params()
-	fichier = open("/amuhome/g11538437/Bureau/m1_s2/IAA/semaine_9/parametres.txt", "w")
+	fichier = open("parametres.txt", "w")
 
 	for item in parametres:
 	     fichier.write("%s \n" % item)
@@ -54,11 +56,14 @@ def question122():
 	X,Y=make_classification(n_samples=1000,n_features=20,n_informative=15,n_classes=3)
 	X_train,X_test,Y_train,Y_test=\
 	train_test_split(X,Y,test_size=0.3,random_state=random.seed())
-	
 	# Question 12222
+
+	print ("--------------------------------------------------------")
+	print ("--             Avant normalisation des donnees         -")
 	print ("--------------------------------------------------------")
 	ACTIVATION_TYPES = ["identity", "logistic", "tanh", "relu"]
 	for activation in ACTIVATION_TYPES:
+		print X_train,"-----------------------------------------------------------------------------------------"
 		print ("activation :", activation)
 		print ("nombre de couches : _1_")
 		start_time = time.time()
@@ -134,7 +139,154 @@ def question122():
 		end_time = time.time()
 		print ("Temps exec : ",str(end_time - start_time))
 		print ("--------------------------------------------------------")
-		
+
+#question1222		
+def ecrireDonnes(fichier,donnees,activation,i,temps,reseau,clsvm,predict,iterReseau,iterCl):
+	donnees.append("---------------------------------------------------------")			
+	donnees.append("-          iteration n "+str(i)+" pour l'activation :"+str(activation)+"    - ")
+	donnees.append("---------------------------------------------------------")	
+	donnees.append("Temps :" +str(temps)+"\n")		
+	donnees.append("Nombre Iterations de Reseau:\n"+str(iterReseau)+"\n")
+	donnees.append("Nombre Iterations de CLSVM:\n"+str(iterCl)+"\n")
+	donnees.append("Performances:\n"+str(predict)+"\n")
 	
+
+
+def question122bis():
+
+	donnees = []
+	i = 0;
+	fichier = open("parametresquestion2.txt", "w")
+
+	#question122bis1
+	iris=load_iris()
+	X,Y=make_classification(n_samples=1000,n_features=20,n_informative=15,n_classes=3)
+	X_train,X_test,Y_train,Y_test=\
+	train_test_split(X,Y,test_size=0.3,random_state=random.seed())
 	
+	scaler = StandardScaler()
+	scaler.fit(X_train)
+	X_train = scaler.transform(X_train)
+	X_test = scaler.transform(X_test)
+
+	# Question 122bis2
+	print ("--------------------------------------------------------")
+	print ("--             Apres normalisation des donnees         -")
+	print ("--------------------------------------------------------")
+	ACTIVATION_TYPES = ["identity", "logistic", "tanh", "relu"]
+	for activation in ACTIVATION_TYPES:
+		print X_train,"-----------------------------------------------------------------------------------------"
+		print ("activation :", activation)
+		print ("nombre de couches : _1_")
+		start_time = time.time()
+		reseau = MLPClassifier(max_iter=200,hidden_layer_sizes=(10), activation=activation, solver='lbfgs', alpha=0.0001, tol=0.0001)
+		reseau.fit(X_train,Y_train)
+		print ("predict :",reseau.predict(X_test))
+		print ("score :",reseau.score(X_train,Y_train))
+		clsvm = svm.SVC(max_iter=200,kernel='poly')
+		clsvm.fit(X_train,Y_train)
+		print ("SVM predict: ",clsvm.predict(X_test))
+		print ("SVM score : ",clsvm.score(X_train,Y_train))
+		end_time = time.time()
+		predict = clsvm.predict(X_test)
+
+		#question 1222
+		ecrireDonnes(fichier,donnees,activation,i,(str(end_time - start_time)),reseau,clsvm,predict,200,200)
+
+		print ("Temps exec : ",str(end_time - start_time))
+		print ("--------------------------------------------------------")
+		print ("activation :", activation)
+		print ("nombre de couches : _2_")
+		start_time = time.time()
+		reseau = MLPClassifier(max_iter=200,hidden_layer_sizes=(10,20), activation=activation, solver='lbfgs', alpha=0.0001, tol=0.0001)
+		reseau.fit(X_train,Y_train)
+		print ("predict :",reseau.predict(X_test))
+		print ("score :",reseau.score(X_train,Y_train))
+		clsvm = svm.SVC(max_iter=200,kernel='poly')
+		clsvm.fit(X_train,Y_train)
+		print ("SVM predict: ",clsvm.predict(X_test))
+		print ("SVM score : ",clsvm.score(X_train,Y_train))
+		end_time = time.time()
+		#question 1222
+		ecrireDonnes(fichier,donnees,activation,i,(str(end_time - start_time)),reseau,clsvm,predict,200,200)
+
+		print ("Temps exec : ",str(end_time - start_time))
+		print ("--------------------------------------------------------")
+		print ("activation :", activation)
+		print ("nombre de couches : _3_")
+		start_time = time.time()
+		reseau = MLPClassifier(max_iter=200,hidden_layer_sizes=(10,20,30), activation=activation, solver='lbfgs', alpha=0.0001, tol=0.0001)
+		reseau.fit(X_train,Y_train)
+		print ("predict :",reseau.predict(X_test))
+		print ("score :",reseau.score(X_train,Y_train))
+		clsvm = svm.SVC(max_iter=200,kernel='poly')
+		clsvm.fit(X_train,Y_train)
+		print ("SVM predict: ",clsvm.predict(X_test))
+		print ("SVM score : ",clsvm.score(X_train,Y_train))
+		end_time = time.time()
+		#question 1222
+		ecrireDonnes(fichier,donnees,activation,i,(str(end_time - start_time)),reseau,clsvm,predict,200,200)
+
+		print ("Temps exec : ",str(end_time - start_time))
+		print ("--------------------------------------------------------")
+		print ("activation :", activation)
+		print ("nombre de couches : _4_")
+		start_time = time.time()
+		reseau = MLPClassifier(max_iter=200,hidden_layer_sizes=(10,20,30,40), activation=activation, solver='lbfgs', alpha=0.0001, tol=0.0001)
+		reseau.fit(X_train,Y_train)
+		print ("predict :",reseau.predict(X_test))
+		print ("score :",reseau.score(X_train,Y_train))
+		clsvm = svm.SVC(max_iter=200,kernel='poly')
+		clsvm.fit(X_train,Y_train)
+		print ("SVM predict: ",clsvm.predict(X_test))
+		print ("SVM score : ",clsvm.score(X_train,Y_train))
+		end_time = time.time()
+		#question 1222
+		ecrireDonnes(fichier,donnees,activation,i,(str(end_time - start_time)),reseau,clsvm,predict,200,200)
+
+		print ("Temps exec : ",str(end_time - start_time))
+		print ("--------------------------------------------------------")
+		print ("activation :", activation)
+		print ("nombre de couches : _5_")
+		start_time = time.time()
+		reseau = MLPClassifier(max_iter=200,hidden_layer_sizes=(10,20,30,40,50), activation=activation, solver='lbfgs', alpha=0.0001, tol=0.0001)
+		reseau.fit(X_train,Y_train)
+		print ("predict :",reseau.predict(X_test))
+		print ("score :",reseau.score(X_train,Y_train))
+		clsvm = svm.SVC(max_iter=200,kernel='poly')
+		clsvm.fit(X_train,Y_train)
+		print ("SVM predict: ",clsvm.predict(X_test))
+		print ("SVM score : ",clsvm.score(X_train,Y_train))
+		end_time = time.time()
+		#question 1222
+		ecrireDonnes(fichier,donnees,activation,i,(str(end_time - start_time)),reseau,clsvm,predict,200,200)
+
+		print ("Temps exec : ",str(end_time - start_time))
+		print ("--------------------------------------------------------")
+
+
+		for item in donnees:
+	     		fichier.write("%s \n" % item)
+
+
+
+#MAIN 
+
+print("####################################################################################")
+print("##		QUESTION 121                                                     ##")
+print("####################################################################################")
+print("creation fichier txt dans la racine")
+
+question121()	
+
+print("####################################################################################")
+print("##		QUESTION 122                                                     ##")
+print("####################################################################################")
+
 question122()
+
+print("####################################################################################")
+print("##		QUESTION 12bis                                                   ##")
+print("####################################################################################")
+
+question122bis()
